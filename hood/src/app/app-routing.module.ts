77dd8bfb99f398ab357d3dca88bd82f29c7262aa
Kasparov <ianjkiplagat@gmail.com>
2021-06-05 15:05:35 +0300

    add routing

--- hood/src/app/app-routing.module.ts
+++ hood/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PostsComponent } from './components/posts/posts/posts.component';
+import { NeighborhoodComponent } from './components/neighborhood/neighborhood/neighborhood.component';
+import { BusinessComponent } from './components/business/business/business.component';
+
+const routes: Routes = [
+  {path: 'businesses', component: BusinessComponent},
+  {path: 'neighborhoods', component: NeighborhoodComponent},
+  {path: 'posts', component: PostsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
