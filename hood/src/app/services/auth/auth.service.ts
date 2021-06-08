d20273ab66cde65abdf14f3f428de7a3ed0e130b
Kasparov <ianjkiplagat@gmail.com>
2021-06-08 14:11:32 +0300

    edit login routing

--- hood/src/app/services/auth/auth.service.ts
+++ hood/src/app/services/auth/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
 
   // tslint:disable-next-line: typedef
   login(model: any): Observable<any>{
-    return this.http.post(this.authUrl + `login/`, model);
+    return this.http.post(this.authUrl + `authlogin/`, model);
   }
   register(model: any): Observable<any>{
     return this.http.post(this.authUrl + `register/`, model);
